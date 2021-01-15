8848b97d400077624a027fe45317412207c29a70
Kenya TANAKA <kenya.5528@gmail.com>
2021-01-15 19:37:19 +0900

    merge gpib functions

---
+++ Functions_Lockin/gpib_init_cond.m
@@ -0,0 +1,41 @@
+function gpib_init_cond(obj)
+
+% Input is A
+fprintf(obj, 'ISRC0');
+% CH1 is R. CH2 is theta.
+fprintf(obj, 'DDEF1,1,0');
+fprintf(obj, 'DDEF2,1,0');
+%GPIB mode
+fprintf(obj, 'OUTX1');
+% read ref. freq.
+Freq = double(deblank(string(query(obj, 'FREQ?'))));
+
+fprintf(obj, 'APHS');
+fprintf(obj, 'DDEF');
+fprintf(obj, 'APHS');
+Out_X = zeros(1,50);
+Out_Y = Out_X;
+Out_Z = Out_X;
+Out_R = Out_X;
+Out_P = Out_X;
+Time = Out_R;
+timer_a = tic;
+for a = 1:60
+    Out_X(a) = double(deblank(string(query(obj, 'OUTP?1'))));
+    Out_Y(a) = double(deblank(string(query(obj, 'OUTP?2'))));
+    Out_R(a) = double(deblank(string(query(obj, 'OUTP?3'))));
+    Out_P(a) = double(deblank(string(query(obj, 'OUTP?4'))));
+    Time(a) = toc(timer_a);
+    disp(a)
+    %     pause(0.1)
+end
+figure(1);
+subplot(2,2,1);title("X");
+plot(Time,Out_X*1000);
+subplot(2,2,2);title("Y");
+plot(Time,Out_Y*1000);
+subplot(2,2,3);title("R");
+plot(Time,Out_R*1000);
+subplot(2,2,4);title("\theta");
+plot(Time,Out_P);
+fclose(obj);
---
+++ Functions_RotStage/RotStageInit.m
@@ -0,0 +1,52 @@
+function sRot = RotStageInit(COMPortNum)
+%RotStageInit To Initialize Rotstage
+%
+%% Instrument Connection
+
+% Find a serial port object.
+sRot = instrfind('Type', 'serial', 'Port', 'COM6', 'Tag', '');
+
+% Create the serial port object if it does not exist
+% otherwise use the object that was found.
+if isempty(sRot)
+    sRot = serial('COM6');
+else
+    fclose(sRot);
+    sRot = sRot(1);
+end
+fopen(sRot);
+
+% Communicating with instrument object, obj1.
+
+% Configure instrument object, obj1.
+set(sRot, 'Terminator', {'CR/LF','CR/LF'});
+set(sRot, 'BaudRate', 38400);
+data1 = query(sRot, 'H:1');
+pause(0.1);
+data2 = query(sRot, 'D:1S500F20000R5000')
+pause(1);
+% data2 = query(obj1, 'M:1+P1000');
+query(sRot, '!:')
+while(true)
+    if(~contains(query(sRot, '!:'),'B'))
+        break;
+    end
+    pause(0.1);
+end
+deg = 0:60:360;
+for c = 1:length(deg)
+    cdeg = deg(c);
+    data3(c) = string(query(sRot, sprintf('A:1+P%4d',cdeg*200)));
+    pause(0.1);
+    data4(c) = string(query(sRot, 'G:'));
+    pause(0.1);
+    while(true)
+        if(~contains(query(sRot, '!:'),'B'))
+            break;
+        end
+        pause(1);
+    end
+end
+
+end
+
