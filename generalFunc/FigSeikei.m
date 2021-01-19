a6ee26984a93a029a6f51c6ca3ea126c599eb32e
Kenya TANAKA <kenya.5528@gmail.com>
2021-01-19 10:35:38 +0900

    Update FigSeikei.m
    support subplot.

--- generalFunc/FigSeikei.m
+++ generalFunc/FigSeikei.m
@@ -2,47 +2,48 @@ function FigSeikei(f)
 if(~exist("f","var"))
     f = gcf;
 end
-a = f.CurrentAxes;
 f.Position(3) = 640;
 f.Position(4) = 480;
-if(f.Position(2)+480 > 1000)
-    f.Position(2) = 1080 - 580;
+for a = f.Children'
+    if(f.Position(2)+480 > 1000)
+        f.Position(2) = 1080 - 580;
+    end
+    if(sum(contains(listfonts,"Helvetica"))==0)
+        a.FontName = 'Arial';
+    else
+        a.FontName = 'Helvetica';
+    end
+    a.FontSize = 12;
+    try
+        a.XColor = [0, 0, 0];
+        a.XLabel.Color = [0, 0, 0];
+    catch
+    end
+    
+    try
+        a.YColor = [0, 0, 0];
+        a.YLabel.Color = [0, 0, 0];
+    catch
+    end
+    
+    try
+        a.ZColor = [0, 0, 0];
+        a.ZLabel.Color = [0, 0, 0];
+    catch
+    end
+    
+    try
+        a.RColor = [0, 0, 0];
+        a.RLabel.Color = [0, 0, 0];
+    catch
+    end
+    try
+        a.ThetaColor = [0, 0, 0];
+        a.ThetaLabel.Color = [0, 0, 0];
+    catch
+    end
+    
+    a.GridColor = [0, 0, 0];
+    a.Box = 'on';
 end
-if(sum(contains(listfonts,"Helvetica"))==0)
-    a.FontName = 'Arial';
-else
-    a.FontName = 'Helvetica';
-end
-a.FontSize = 12;
-try
-    a.XColor = [0, 0, 0];
-    a.XLabel.Color = [0, 0, 0];
-catch
-end
-
-try
-    a.YColor = [0, 0, 0];
-    a.YLabel.Color = [0, 0, 0];
-catch
-end
-
-try
-    a.ZColor = [0, 0, 0];
-    a.ZLabel.Color = [0, 0, 0];
-catch
-end
-
-try
-    a.RColor = [0, 0, 0];
-    a.RLabel.Color = [0, 0, 0];
-catch
-end
-try
-    a.ThetaColor = [0, 0, 0];
-    a.ThetaLabel.Color = [0, 0, 0];
-catch
-end
-
-a.GridColor = [0, 0, 0];
-a.Box = 'on';
 end
