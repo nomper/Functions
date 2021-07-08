72e4f3fbd236944df5c0d411185405d6af581bdb
Kenya TANAKA <kenya.5528@gmail.com>
2021-07-08 14:15:50 +0900

    Update print_tanaka2.m
    add function of optipng.

--- generalFunc/print_tanaka2.m
+++ generalFunc/print_tanaka2.m
@@ -1,6 +1,9 @@
-function print_tanaka2(figurehandle, mFilename, bgs)
+function print_tanaka2(figurehandle, mFilename, bgs, opti)
 filename = sprintf("%s_fig%02d",mFilename, figurehandle.Number);
 if(~exist("bgs","var"))
     bgs = false;
 end
-print_tanaka(figurehandle, filename, bgs)
+if(~exist("opti","var"))
+    opti = false;
+end
+print_tanaka(figurehandle, filename, bgs, opti)
