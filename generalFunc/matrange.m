fe1c106f241915a8215b7163f326cac0c1943d7d
Kenya TANAKA <kenya.5528@gmail.com>
2022-01-05 15:30:25 +0900

    Update matrange.m
    Auto switch, when min > max

--- generalFunc/matrange.m
+++ generalFunc/matrange.m
@@ -1,4 +1,11 @@
 function r = matrange(x, minvalue, maxvalue)
+%% min < x and x < max のboolen を返却します。
+if(minvalue > maxvalue)
+    tempvalue = maxvalue;
+    maxvalue = minvalue;
+    minvalue = tempvalue;
+end
 r = and(minvalue < x, x < maxvalue);
+
 end
 
