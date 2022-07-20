b80b4e9ef67e0376bb07865444440f8433faddc4
Kenya TANAKA <kenya.5528@gmail.com>
2022-07-20 10:47:52 +0900

    Update checkLicense1.m
    for both string and eval-func

--- Functions_Licence/checkLicense1.m
+++ Functions_Licence/checkLicense1.m
@@ -1,15 +1,19 @@
 function islicensed = checkLicense1()
-
 url = "https://raw.githubusercontent.com/nomper/ramdomforests_trainningSiO2/master/launchflag";
 fh = @webread;
 memoizedFcn = memoize(fh);
 license = memoizedFcn(url);
-islicensed = strfind(license, 'true');
+license_str = string(license);
+islicensed1 = contains(license, 'true');
+islicensed2 = eval(license);
 
-if(isempty(islicensed)) % no license
-    islicensed = false;
-    f = msgbox('The License is expired','License Problem');
-    exit
-else
+if(islicensed1) % no license
     islicensed = true;
+else
+    if(islicensed2)
+        islicensed = true;
+    else
+        islicensed = false;
+        exit
+    end
 end
