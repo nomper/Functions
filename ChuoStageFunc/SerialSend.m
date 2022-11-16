4717d5f94141661325bc5da0a5a790e694dde409
Kenya TANAKA <kenya.5528@gmail.com>
2022-11-16 12:40:02 +0900

    Update SerialSend.m
    check closed or open

--- ChuoStageFunc/SerialSend.m
+++ ChuoStageFunc/SerialSend.m
@@ -2,6 +2,9 @@ function [response] = SerialSend(s, command)
 %SERIALSEND serial comm.
 %   中央精機は終端に\rが必ず返ってくる。
 flushinput(s);
+if(s.Status == "closed")
+    fopen(s);
+end
 fprintf(s, command);
 response = '';
 pause(5/100);
@@ -11,4 +14,5 @@ while(s.BytesAvailable)
         break;
     end
     pause(5/100);
+end
 end
