89a63f2717330002f3fb3a9057bc1b6e099f0f09
Kenya TANAKA <kenya.5528@gmail.com>
2022-11-16 12:45:28 +0900

    Update lockin_readXYRT.m
    fast return in N=1 case

--- Functions_Lockin/lockin_readXYRT.m
+++ Functions_Lockin/lockin_readXYRT.m
@@ -2,16 +2,25 @@ function [X, Y, R, T] = lockin_readXYRT(obj, N)
 if(~exist("N","var"))
     N = 1;
 end
+
+if(N == 1)
+    X = double(string(query(obj, 'OUTP?1')));
+    Y = double(string(query(obj, 'OUTP?2')));
+    R = double(string(query(obj, 'OUTP?3')));
+    T = double(string(query(obj, 'OUTP?4')));
+    return;
+end
+
 Xt = zeros(1,N);
 Yt = zeros(1,N);
 Rt = zeros(1,N);
 Tt = zeros(1,N);
 
 for n = 1:N
-    Xt(n) = double(deblank(string(query(obj, 'OUTP?1'))));
-    Yt(n) = double(deblank(string(query(obj, 'OUTP?2'))));
-    Rt(n) = double(deblank(string(query(obj, 'OUTP?3'))));
-    Tt(n) = double(deblank(string(query(obj, 'OUTP?4'))));
+    Xt(n) = double(string(query(obj, 'OUTP?1')));
+    Yt(n) = double(string(query(obj, 'OUTP?2')));
+    Rt(n) = double(string(query(obj, 'OUTP?3')));
+    Tt(n) = double(string(query(obj, 'OUTP?4')));
 end
 X = mean(Xt);
 Y = mean(Yt);
