2a88e4d0011814fe04611747460d8af653c8ecfb
Kenya TANAKA <kenya.5528@gmail.com>
2023-01-27 21:00:09 +0900

    Update StageWait.m
    update for no response case

--- ChuoStageFunc/StageWait.m
+++ ChuoStageFunc/StageWait.m
@@ -1,10 +1,24 @@
-function StageWait(s)
+function StageWait(s,t)
 %StageWait Waiting for mov.
 %   詳細説明をここに記述
+timer = tic;
+if(~exist("t","var"))
+    t = 2;
+end
 while(true)
     Rep = SerialSend(s,'Q:2');
-    if(sum(Rep=='K') > 3)
+    if(isempty(Rep))
+        continue;
+    
+    end
+
+    if(count(Rep,"K") > 3)
         break
     end
+    if(toc(timer)>t)
+        toc
+        break;
+    end
 end
+clear timer
 end
