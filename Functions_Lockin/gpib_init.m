7da74d3eb2adbff1514b86b89558b57805829f8d
Kenya TANAKA <kenya.5528@gmail.com>
2023-01-27 21:02:31 +0900

    update for visadev function
    using visadev func. instead of gpib func.

--- Functions_Lockin/gpib_init.m
+++ Functions_Lockin/gpib_init.m
@@ -1,5 +1,7 @@
 function obj = gpib_init()
-obj = gpib('ni',0,11,"EOSCharCode", 'LF', "EOSMode", 'read&write');
-fopen(obj);
-IDN = query(obj, "*IDN?")
+% obj = gpib('ni',0,11,"EOSCharCode", 'LF', "EOSMode", 'read&write');
+obj = visadev("GPIB0::11::INSTR");
+% fopen(obj);
+IDN = query(obj, "*IDN?");
+fprintf(IDN+"\n");
 end
